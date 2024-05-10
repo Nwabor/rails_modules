ecdf4416be28b6b936f463977bf0a47d402c605c
Nwabor <knwabor@gmail.com>
2024-05-10 13:20:09 +0100

    rails as a module

--- modules/rails/controllers/application_controller.rb
+++ modules/rails/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+   # find views within our modules
+   append_view_path(Dir.glob(Rails.root.join("modules/*/views")))
 end
