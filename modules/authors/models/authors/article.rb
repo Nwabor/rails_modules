43994c4378bbd5a5f8615c9fea5227485f36fee4
Nwabor <knwabor@gmail.com>
2024-05-10 15:47:34 +0100

    authors is now a protected module

--- modules/authors/models/authors/article.rb
+++ modules/authors/models/authors/article.rb
@@ -1,3 +1,6 @@
-class Authors::Article < ApplicationRecord
+module Authors
+  class Article < ApplicationRecord
   belongs_to :authors_user, class_name: 'Authors::User'
+  end
+  private_constant :Article
 end
--- modules/authors/models/authors/user.rb
+++ modules/authors/models/authors/user.rb
@@ -1,3 +1,6 @@
-class Authors::User < ApplicationRecord
-  has_many :authors_articles, class_name: 'Authors::Article', foreign_key: 'authors_user_id', dependent: :destroy
+module Authors
+  class User < ApplicationRecord
+    has_many :authors_articles, class_name: 'Authors::Article', foreign_key: 'authors_user_id', dependent: :destroy
+  end
+  private_constant :User
 end
